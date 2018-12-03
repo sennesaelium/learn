2c385a6c84dcd5bb89b70a4423d160ad40dc09ae
vincent sennesael <sennesvi@macbook-vincent.home>
2018-12-04 00:12:23 +0100

    versie 1

---
+++ play.js
@@ -0,0 +1,7 @@
+var dohet = x => {
+  return x * x;
+};
+
+y = dohet(4);
+
+console.log(dohet(5));
---
+++ test.js
@@ -0,0 +1,8 @@
+function add (x, y) {
+  return x + y
+}
+function addFive (x, addReference) {
+  return addReference(x, 5) // 15 - Press the button, run the machine.
+}
+addFive(10, add);//? 
+
